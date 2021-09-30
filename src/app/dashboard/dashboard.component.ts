895b3f7967484f50c3d5511947abacb0c2e6d188
Martin Smebye <mbsmebye@gmail.com>
2021-09-30 12:02:56 +0200

    Update dashboard component to recieve boligmappaNumber

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(private elementRef: ElementRef) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.elementRef.nativeElement
+      .querySelector('boligmappa-search')
+      .addEventListener('property-confirmed', this.exampleFunction.bind(this));
   }
 
+  exampleFunction(e: CustomEvent) {
+    const boligmappaNumber = e.detail.boligmappaNumber;
+
+    //...
+  }
 }
